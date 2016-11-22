9c40bffdd559801ffea3930c9ca2f147be834b18
Huw Jones <huwcbjones@gmail.com>
2016-11-22 16:07:39 +0000

    Updated summary and produced new summary files

--- Exercise_2/summary.sh
+++ Exercise_2/summary.sh
@@ -7,16 +7,35 @@ do
     RS = "\n";
     FS = ",";
     recordNum = 0;
+    min = 9999999;
+    maxi = 0;
     average = 0;
+    status = "success";
   }
   {
     if(recordNum != 0){
-      average=average + $4
+      if($4 == " 0 received") {
+        status = "Blocked";
+      } else if ($4 == "") {
+        status = "No route to host";
+      } else {
+        average=average + $4;
+        if($3 < min){
+          min = $3
+        }
+        if($5 > maxi){
+          maxi = $5
+        }
+      }
     }
     recordNum++;
   }
   END {
-    printf("'$host',%.3f\n",(average/recordNum));
+    if(status != "success") {
+      printf("'$host',,,,%s\n", status);
+    } else {
+      printf("'$host',%.3f,%.3f,%.3f,%s\n",min,maxi,(average/recordNum), status);
+    }
   }
   ' $file
 done
