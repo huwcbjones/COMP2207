95db39cb11f18e2740e33a935d24bb078ab0b2dc
Huw Jones <huwcbjones@gmail.com>
2016-12-03 19:52:04 +0000

    Updated StartRMI.bat to only start rmiregistry if it's not already running

--- Main_Coursework/Clock.bat
+++ Main_Coursework/Clock.bat
@@ -1,3 +1,7 @@
 @echo off
-(tasklist /nh /fi "imagename eq rmiregistry.exe" | find /i "rmiregistry.exe" > nul || (CALL StartRMI.bat))
-java -cp "out\production\Main_Coursework" -Djava.rmi.server.codebase=file:/E:/Documents/University/COMP2207/Main_Coursework/out/Production/Main_Coursework/ Clock %*
+(CALL StartRMI.bat)
+java -cp "out\production\Main_Coursework" ^
+	-Djava.rmi.server.codebase=file:/C:/Users/Huw/Documents/University/COMP2207/Main_Coursework/out/Production/Main_Coursework/ ^
+	-Djava.rmi.server.hostname=localhost ^
+	-Djava.security.policy=server.policy ^
+	Clock %*
--- Main_Coursework/StartRMI.bat
+++ Main_Coursework/StartRMI.bat
@@ -1,6 +1,8 @@
 @echo off
-pushd %cd%
-CD /D "C:\Program Files\Java\jdk1.8.0_101\bin"
-start /B rmiregistry.exe
-popd %cd%
-CD /D %cd%
+(tasklist /nh /fi "imagename eq rmiregistry.exe" | find /i "rmiregistry.exe" > nul || (
+	pushd %cd%
+	CD /D "C:\Program Files\Java\jdk1.8.0_101\bin"
+	start /B rmiregistry.exe
+	popd %cd%
+	CD /D %cd%
+))
