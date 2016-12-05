40259405948f90088b061f941a03a9f7f67cd7a0
Huw Jones <huwcbjones@gmail.com>
2016-12-05 16:05:19 +0000

    Fixed scripts

--- Main_Coursework/Clock.bat
+++ Main_Coursework/Clock.bat
@@ -1,7 +1,11 @@
 @echo off
-(CALL StartRMI.bat)
+pushd %cd%
+CD /D "E:\Documents\University\COMP2207\Main_Coursework\out\Production\Main_Coursework"
+(CALL E:\Documents\University\COMP2207\Main_Coursework\StartRMI.bat)
+popd %cd%
+CD /D %cd%
 java -cp "out\production\Main_Coursework" ^
-	-Djava.rmi.server.codebase=file:/C:/Users/Huw/Documents/University/COMP2207/Main_Coursework/out/Production/Main_Coursework/ ^
+	-Djava.rmi.server.codebase=file:/E:/Documents/University/COMP2207/Main_Coursework/out/production/Main_Coursework/ ^
 	-Djava.rmi.server.hostname=localhost ^
 	-Djava.security.policy=server.policy ^
 	Clock %*
--- Main_Coursework/StartRMI.bat
+++ Main_Coursework/StartRMI.bat
@@ -1,8 +1,4 @@
 @echo off
 (tasklist /nh /fi "imagename eq rmiregistry.exe" | find /i "rmiregistry.exe" > nul || (
-	pushd %cd%
-	CD /D "C:\Program Files\Java\jdk1.8.0_101\bin"
 	start /B rmiregistry.exe
-	popd %cd%
-	CD /D %cd%
 ))
