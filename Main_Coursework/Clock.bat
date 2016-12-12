80c798e707ca3be175d01685ea6f1b3c5ee00ac2
Huw Jones <huwcbjones@gmail.com>
2016-12-12 21:17:25 +0000

    Made file paths relative

--- Main_Coursework/Clock.bat
+++ Main_Coursework/Clock.bat
@@ -1,10 +1,10 @@
 @echo off
 pushd %cd%
-CD /D "E:\Documents\University\COMP2207\Main_Coursework\out\Production\Main_Coursework"
-(CALL E:\Documents\University\COMP2207\Main_Coursework\StartRMI.bat)
+CD /D "out\Production\Main_Coursework"
+(CALL ..\..\..\StartRMI.bat)
 popd %cd%
 CD /D %cd%
 java -cp "out\production\Main_Coursework" ^
-	-Djava.rmi.server.codebase=file:/E:/Documents/University/COMP2207/Main_Coursework/out/production/Main_Coursework/ ^
+	-Djava.rmi.server.codebase=file:out/production/Main_Coursework/ ^
 	-Djava.security.policy=server.policy ^
 	Clock %*
